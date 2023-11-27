6a22d3a03cab029fa11ab83b10539dcffac59dec
ibnu fadhil <fadhilgelaws@gmail.com>
2023-11-28 00:15:38 +0700

    some fixes

--- database/obat-kita.sql
+++ database/obat-kita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 03:21 PM
+-- Generation Time: Nov 27, 2023 at 04:08 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,8 +18,150 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `obat-kita`
+-- Database: `obat_kita`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+CREATE TABLE `brands` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(255) NOT NULL,
+  `brand_active` int(11) NOT NULL DEFAULT 0,
+  `brand_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`brand_id`, `brand_name`, `brand_active`, `brand_status`) VALUES
+(1, 'Bodrex', 1, 1),
+(2, 'Panadol', 1, 1),
+(3, 'Saripati Ayam', 1, 1),
+(4, 'Redoxon', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `categories_id` int(11) NOT NULL,
+  `categories_name` varchar(255) NOT NULL,
+  `categories_active` int(11) NOT NULL DEFAULT 0,
+  `categories_status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`categories_id`, `categories_name`, `categories_active`, `categories_status`) VALUES
+(1, 'Tablets', 1, 1),
+(2, 'Syrup', 1, 1),
+(3, 'Supplement', 1, 1),
+(4, 'PainKiller', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(15) NOT NULL,
+  `uno` varchar(50) NOT NULL,
+  `orderDate` date NOT NULL,
+  `clientName` text NOT NULL,
+  `projectName` varchar(30) NOT NULL,
+  `clientContact` varchar(15) NOT NULL,
+  `address` varchar(30) NOT NULL,
+  `subTotal` int(100) NOT NULL,
+  `totalAmount` int(100) NOT NULL,
+  `discount` int(100) NOT NULL,
+  `grandTotalValue` int(100) NOT NULL,
+  `gstn` int(100) NOT NULL,
+  `paid` int(100) NOT NULL,
+  `dueValue` int(100) NOT NULL,
+  `paymentType` int(15) NOT NULL,
+  `paymentStatus` int(15) NOT NULL,
+  `paymentPlace` int(5) NOT NULL,
+  `delete_status` tinyint(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `uno`, `orderDate`, `clientName`, `projectName`, `clientContact`, `address`, `subTotal`, `totalAmount`, `discount`, `grandTotalValue`, `gstn`, `paid`, `dueValue`, `paymentType`, `paymentStatus`, `paymentPlace`, `delete_status`) VALUES
+(1, 'INV-0001', '2022-02-28', 'Ibnu Fadhil', '', '123345648', '', 100, 10, 108, 49, 0, 49, 49, 2, 1, 0, 0),
+(5, 'INV-0005', '2023-11-27', 'Ibnu', '', '1234235432', '', 30, 33, 0, 33, 3, 33, 0, 2, 2, 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_item`
+--
+
+CREATE TABLE `order_item` (
+  `id` int(15) NOT NULL,
+  `productName` int(100) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `rate` varchar(255) NOT NULL,
+  `total` varchar(255) NOT NULL,
+  `lastid` int(50) NOT NULL,
+  `added_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `order_item`
+--
+
+INSERT INTO `order_item` (`id`, `productName`, `quantity`, `rate`, `total`, `lastid`, `added_date`) VALUES
+(5, 2, '1', '100', '100.00', 1, '0000-00-00'),
+(6, 2, '2', '150', '300.00', 2, '0000-00-00'),
+(7, 1, '2', '30', '60.00', 3, '2022-04-15'),
+(8, 2, '4', '150', '600.00', 3, '2022-04-15'),
+(9, 3, '1', '200', '200.00', 3, '2022-04-15'),
+(10, 1, '2', '30', '60.00', 4, '2022-04-15'),
+(11, 1, '1', '30', '30.00', 5, '2023-11-27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `product_image` text NOT NULL,
+  `brand_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `quantity` varchar(255) NOT NULL,
+  `rate` varchar(255) NOT NULL,
+  `mrp` int(100) NOT NULL,
+  `bno` varchar(50) NOT NULL,
+  `expdate` date NOT NULL,
+  `added_date` date NOT NULL,
+  `active` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`product_id`, `product_name`, `product_image`, `brand_id`, `categories_id`, `quantity`, `rate`, `mrp`, `bno`, `expdate`, `added_date`, `active`, `status`) VALUES
+(1, 'Paracetamol', 'Bodrex2.jpg', 1, 1, '50', '30', 40, '307002', '2022-02-28', '2022-02-28', 1, 1),
+(2, 'Obat Sakit Kepala', 'Panadol-Extra.jpg', 2, 1, '30', '150', 200, '307003', '2022-02-16', '2022-02-28', 1, 1),
+(3, 'Obat Pernapasan', 'saripati ayam.jpg', 3, 3, '70', '200', 300, '307004', '2022-03-13', '2022-02-28', 1, 1),
+(4, 'Vitamin C', 'redoxon.webp', 4, 1, '500', '25', 30, '307005', '2022-05-31', '2022-04-15', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -46,6 +188,36 @@ INSERT INTO `users` (`user_id`, `username`, `password`, `email`) VALUES
 --
 
 --
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categories_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_item`
+--
+ALTER TABLE `order_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`product_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -54,6 +226,36 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `categories_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order_item`
+--
+ALTER TABLE `order_item`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
